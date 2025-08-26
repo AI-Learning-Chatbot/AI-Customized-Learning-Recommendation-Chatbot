f1d0e567178fd084f4ba51ebdff12bf6372593aa
Aaryan Shakya <nayranewar33@gmail.com>
2025-08-26 14:48:58 +0000

    index page modify

--- test/main.py
+++ test/main.py
@@ -140,7 +140,7 @@ def reset():
 
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=5000, debug=True)
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    #app.run(host="127.0.0.1", port=5001, debug=True)
 
 
